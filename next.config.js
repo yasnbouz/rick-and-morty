2bd1ccabd493bf2776bff219176a72bec1896422
yasnbouzi <elbouziady.yassine@gmail.com>
2021-02-08 17:00:50 +0100

    feat: ✨ Add PWA

--- next.config.js
+++ next.config.js
@@ -2,10 +2,16 @@ const withPreact = require('next-plugin-preact');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 const nextConfig = {
   images: {
     domains: ['rickandmortyapi.com'],
+  },
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
   },
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
@@ -16,4 +22,4 @@ const nextConfig = {
     return config;
   },
 };
-module.exports = withBundleAnalyzer(withPreact(nextConfig));
+module.exports = withBundleAnalyzer(withPWA(withPreact(nextConfig)));
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,7 +25,15 @@ class MyDocument extends Document<Props> {
           <link rel="preload" href="/fonts/Poppins/Poppins-Medium.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
           <link rel="preload" href="/fonts/Poppins/Poppins-SemiBold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
           <link rel="preload" href="/fonts/Poppins/Poppins-Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
-          <link href="/favicon.ico" rel="icon" type="image/x-icon" />
+          {/* meta tags PWA */}
+          <meta name="theme-color" content="#333" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" type="image/png" href="/icons/favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/icons/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/icons/pwa-192x192.png" sizes="192x192" />
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon-180x180.png" sizes="180x180" />
+          <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+          <link rel="mask-icon" href="/icons/maskable-icon.png" />
         </Head>
         <body>
           <Main />
