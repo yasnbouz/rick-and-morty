a57aa507e99dfc6bb14455748dbef0373f331a2c
yasnbouzi <elbouziady.yassine@gmail.com>
2021-02-25 16:36:29 +0100

    refactor(footer): update github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,8 @@ export default function Footer() {
         <HeartIcon />
         <p>
           by&nbsp;
-          <a href="https://github.com/yasnbouzi" target="_blank" rel="noreferrer noopener" aria-label="developer github">
-            @yasnbouzi
+          <a href="https://github.com/yasnbouz" target="_blank" rel="noreferrer noopener" aria-label="developer github">
+            @yasnbouz
           </a>
         </p>
       </>
