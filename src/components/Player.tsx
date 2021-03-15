94f4c03dcf07dcf4f640dd3a245f88fd94d24d32
yasnbouzi <elbouziady.yassine@gmail.com>
2021-03-15 19:35:40 +0100

    fix(player): prevent scrolling to top after exit full screen

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from 'preact/hooks';
+
 export default function Player({ url, title }: { url: string; title: string }) {
+  useEffect(() => {
+    const scrollToTop = () => {
+      if (!document.fullscreenElement) {
+        document.getElementById(`episodes`).scrollIntoView({ behavior: `auto` });
+      }
+    };
+    document.addEventListener(`fullscreenchange`, scrollToTop);
+    return () => {
+      document.removeEventListener(`fullscreenchange`, scrollToTop);
+    };
+  }, []);
+
   return (
     <div className="aspect-ratio" style={{ backgroundColor: `black` }}>
       <iframe sandbox="allow-scripts" src={url} scrolling="no" frameBorder="1" height="100%" width="100%" allowFullScreen title={title} />
