d47e45d26302bb159f43743c9b7f27db5b60f214
Yassine El Bouziady <elbouziady.yassine@gmail.com>
2022-03-23 23:07:53 +0100

    fix: image margin

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -142,7 +142,7 @@ export const StyledHeroContainer = styled.div`
   ${containerStyle}
 `;
 export const StyledPoster = styled.div`
-  ${tw`mx-auto block w-full max-w-[310px] max-h-[500px] flex-shrink-0 mb-5 md:(mr-10 mb-0)`}
+  ${tw`mx-auto block w-full max-w-[310px] max-h-[500px] flex-shrink-0 mb-5 md:(ml-0 mr-10 mb-0)`}
 `;
 export const StyledContent = styled.div`
   ${tw`flex flex-col items-start text-white`}
